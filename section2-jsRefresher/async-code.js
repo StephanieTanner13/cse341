8ba7c7f531ba3311168840180cbf4c716ad2de54
StephanieTanner13 <80174471+StephanieTanner13@users.noreply.github.com>
2022-01-25 19:44:45 -0700

    Updated File Structure
    Final Project to Deploy

--- section2-jsRefresher/async-code.js
+++
@@ -1,8 +0,0 @@
-//this code is asyncronous code (it doesn't happen immediately)
-setTimeout(() => {
-    console.log('Timer is done!');
-}, 2000);
-
-//syncronous code all happens at the same time
-console.log('Hello!');
-console.log('Hi!');
